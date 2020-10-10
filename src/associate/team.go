33daae9495c6c91dda7a88d0571a472582423911
leglesslamb <wu.rupert@outlook.com>
2020-10-10 18:06:29 -0400

    Completed generalizing new backslash sub pattern, addressing #13.

--- src/associate/team.go
+++ src/associate/team.go
@@ -8,23 +8,27 @@ import (
 func AddTeamEntries(
 	addEntries []Entry,
 	entries map[string][]Entry) map[string][]Entry {
-	for _, entry := range addEntries {
 
+	for _, entry := range addEntries {
 		titles := strings.Split(entry.Position, ",")
 		for _, title := range titles {
-			if strings.Contains(title, "(") && strings.Contains(title, ")") {
-				start := strings.Index(title, "(")
-				end := strings.Index(title, ")")
-				team := title[start+1 : end]
-				position := strings.TrimSpace(title[:start])
-				entry.Position = position
-				entry.Department = team
-				if _, ok := entries[team]; ok {
-					entries[team] = append(entries[team], entry)
-				} else {
-					entries[team] = []Entry{entry}
-				}
+			if !strings.Contains(title, "(") ||
+				!strings.Contains(title, ")") {
+				continue
 			}
+
+			start := strings.Index(title, "(")
+			end := strings.Index(title, ")")
+
+			position := strings.TrimSpace(title[:start])
+			team := title[start+1 : end]
+			entry.Position = position
+			entry.Department = team
+
+			if _, ok := entries[team]; !ok {
+				entries[team] = []Entry{}
+			}
+			entries[team] = append(entries[team], entry)
 		}
 	}
 	return entries
